5cc453d0da8337bf9152fc5610c47a216a9e68b6
Sylvain Gable <sylvain@vingtcinq.io>
2024-04-19 00:21:41 +0200

    Add the ability view past turns and edit last one

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const gameDataKey = "gameData";
-const teamInitialData = {
-  name: "",
-  turns: [],
-};
 const gameInitialData = {
-  teams: { 1: teamInitialData, 2: teamInitialData },
+  teams: { 1: "", 2: "" },
+  turns: [],
   isGameStarted: false,
-  isGameFinished: false,
+  startingTeam: null,
   includeBullsEye: false,
 };
 const emptyTurn = {
@@ -27,35 +24,64 @@ function setGameData(data) {
   sessionStorage.setItem(gameDataKey, JSON.stringify(data));
 }
 
-function resetGameData() {
-  gameData = getGameData();
-  gameData.teams[1].turns = [];
-  gameData.teams[2].turns = [];
+function restartGame() {
+  let gameData = getGameData();
+  // Keep the teams and game config but reset the turns
+  gameData.turns = [];
   setGameData(gameData);
 }
 
 function startGame(firstTeamName, secondTeamName, includeBullsEye) {
   const data = getGameData();
-  data.teams[1].name = firstTeamName;
-  data.teams[2].name = secondTeamName;
+  data.teams[1] = firstTeamName;
+  data.teams[2] = secondTeamName;
   data.includeBullsEye = includeBullsEye;
   data.isGameStarted = true;
-  data.isGameFinished = false;
   setGameData(data);
 }
 
-function addTurn(team, turn) {
+function setTurn(team, turn, turnIndex) {
   const data = getGameData();
-  data.teams[team].turns.push(turn);
+  // Set the starting team on the first turn
+  if (data.turns.length === 0 && data.startingTeam === null) {
+    data.startingTeam = team;
+  }
+  if (turnIndex !== undefined) {
+    data.turns[turnIndex] = turn;
+  } else {
+    data.turns.push(turn);
+  }
   setGameData(data);
 }
 
-function getNumberMark(team, number) {
-  const gameData = getGameData();
-  const hits = gameData.teams[team].turns.reduce(
-    (acc, turn) => acc + turn[number],
-    0
-  );
+function getPlayedTurns(asOfTurnIndex = undefined) {
+  // Return all the turns already played up to the given index. For example, with
+  // asOfTurnIndex = 2, it return the first 2 turns (considering the third one hasn't
+  // been played yet).
+  const data = getGameData();
+  if (asOfTurnIndex === undefined) {
+    return data.turns;
+  }
+  return data.turns.slice(0, asOfTurnIndex);
+}
+
+function getTeamTurns(turns, team) {
+  const startingTeam = getGameData().startingTeam;
+  // Starting team turns are the even indexes, the other team turns are the odd indexes
+  const filterPredicate =
+    startingTeam === team
+      ? (_, index) => index % 2 === 0
+      : (_, index) => (index + 1) % 2 === 0;
+  return turns.filter(filterPredicate);
+}
+
+function getNumberMark(teamTurns, number) {
+  // Return a mark for a number according to cumulative hits for this number:
+  //  - 0 hit: ""
+  //  - 1 hit: "/"
+  //  - 2 hits: "X"
+  //  - 3 hits or more: "O"
+  const hits = teamTurns.reduce((acc, turn) => acc + turn[number], 0);
   switch (hits) {
     case 0:
       return "";
@@ -68,9 +94,9 @@ function getNumberMark(team, number) {
   }
 }
 
-function sumTeamTurns(team) {
-  const gameData = getGameData();
-  return gameData.teams[team].turns.reduce(
+function sumTeamTurns(teamTurns) {
+  // Sum all the turns of a team to get the total hits for each number
+  return teamTurns.reduce(
     (acc, turn) => {
       Object.entries(turn).forEach(([key, value]) => {
         acc[key] = (acc[key] || 0) + value;
@@ -89,28 +115,28 @@ function sumTeamTurns(team) {
   );
 }
 
-function getAvgHitsPerTurn(team) {
-  const turnsSum = sumTeamTurns(team);
+function getAvgHitsPerTurn(teamTurns) {
+  const turnsSum = sumTeamTurns(teamTurns);
   const totalHits = Object.values(turnsSum).reduce((acc, value) => acc + value);
-  return Number(totalHits / getGameData().teams[team].turns.length).toFixed(1);
+  return Number(totalHits / teamTurns.length).toFixed(1);
 }
 
-function countEmptyTurns(team) {
-  return getGameData().teams[team].turns.filter((turn) =>
+function countEmptyTurns(teamTurns) {
+  return teamTurns.filter((turn) =>
     Object.values(turn).every((value) => value === 0)
   ).length;
 }
 
-function getTeamTotalPoints(team) {
-  const turnsSum = sumTeamTurns(team);
+function getTeamTotalPoints(teamTurns) {
+  const turnsSum = sumTeamTurns(teamTurns);
   return Object.keys(turnsSum).reduce(
     (acc, key) => acc + (Math.max(turnsSum[key], 3) - 3) * key,
     0
   );
 }
 
-function hasWon({ team, includeBullsEye }) {
-  const turnsSum = sumTeamTurns(team);
+function hasWon({ teamTurns, includeBullsEye }) {
+  const turnsSum = sumTeamTurns(teamTurns);
   let requiredSum = {
     20: 3,
     19: 3,
